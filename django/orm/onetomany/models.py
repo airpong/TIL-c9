da8e9dab8dbd8433690abca29540d91233a335ee
Changun Ha <giponge@gmail.com>
2019-04-18 07:44:59 +0000

    orm N:M

--- django/orm/onetomany/models.py
+++ django/orm/onetomany/models.py
@@ -43,6 +43,25 @@ class Comment(models.Model):
 
 # 8. 댓글 중에 해당 게시글의 제목에 '1'이 들어가 있는 것은?
 # Comment.objects.filter(post__title__contains='1')
+
+# 병원에 오는 사람들을 기록하는 시스템을 만드려고 한다.
+# 필수적인 모델은 환자와 의사이다.
+# 어떠한 관계로 표현할 수 있을까?
+
+class Doctor(models.Model):
+    name = models.TextField()     
+
+# Doctor:Patient = 1:N    
+class Patient(models.Model):
+    name = models.TextField()
+    doctors = models.ManyToManyField(Doctor, related_name='patients')
+
+    
+# class Reservation(models.Model):
+#     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+#     patient = models.ForeignKey(Patient,on_delete=models.CASCADE)
+
+
 # user1 = User.objects.create(name='Kim')
 # user2 = User.objects.create(name='Lee')
 # post1 = Post.objects.create(title='1글',user=user1)
@@ -55,4 +74,9 @@ class Comment(models.Model):
 # c5 = Comment.objects.create(content='2글1댓',user=user1,post=post2)
 # c6 = Comment.objects.create(content='!1글5댓',user=user2,post=post1)
 # c7 = Comment.objects.create(content='!2글2댓',user=user2,post=post2)
-
+# doctor1 = Doctor.objects.create(name='Kim')
+# doctor2 = Doctor.objects.create(name='Kang')
+# patient1 = Patient.objects.create(name='Tom')
+# patient2 = Patient.objects.create(name='John')
+# Reservation.objects.create(doctor=doctor1,patient=patient2)
+# Reservation.objects.create(doctor=doctor1,patient=patient1)
